8a397bf2f7bb3b58734e1fdc8a237f99432942ca
AndreiMikh <andreimikhail0087@gmail.com>
2026-03-25 22:42:01 +0800

    Update service.sh

--- zygisk/module/service.sh
+++ zygisk/module/service.sh
@@ -1,6 +1,6 @@
-# Extract the directory path and change directory 
+# Extract the Directory Path and Change Directory 
 MODDIR="${0%/*}"
 cd "$MODDIR" || exit 1
 
-# Start the daemon directly in the background within a private mount namespace
+# Start the Daemon Directly in the Background within a Private Mount Namespace
 unshare --propagation slave -m "$MODDIR/daemon" --system-server-max-retry=3 "$@" &
